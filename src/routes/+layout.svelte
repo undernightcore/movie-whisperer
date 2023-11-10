95f0b33503b12020a0d5380b52adb41b8bff5e04
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-11-10 17:33:37 +0100

    feat(ui): add view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,18 @@
+<script>
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
+</script>
+
 <slot />
 
 <style lang="scss">
